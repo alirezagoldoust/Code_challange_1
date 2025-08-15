89d3cef8f431c51fd2709dd4f0d546045144dda2
dibadokht <rdibadokht@gmail.com>
2025-08-15 02:25:49 -0700

    start

--- display.py
+++ display.py
@@ -10,3 +10,16 @@ def print_board(dict_1):
     print('| ' + ' | '.join(dict_1[6]) + ' |')
     print('| ' + ' | '.join(dict_1[7]) + ' |')
         
+        
+input_1 = input("if you want to play type start: ")
+
+if input_1 == "start":
+    print("start ")
+    player_1 = input("player1: enter your name:")
+    player_2 = input("player2: enter your name:")
+
+
+
+
+
+
