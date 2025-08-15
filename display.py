ae0655dd16546143f96753304cc28a2a16121139
dibadokht <rdibadokht@gmail.com>
2025-08-15 02:05:11 -0700

    print_board

--- display.py
+++ display.py
@@ -1,2 +1,12 @@
 # This file contain game board diplaying.
 # 🙌 Good job
+
+def print_board(dict_1):
+    print('| ' + ' | '.join(dict_1[1]) + ' |')
+    print('| ' + ' | '.join(dict_1[2]) + ' |')
+    print('| ' + ' | '.join(dict_1[3]) + ' |')
+    print('| ' + ' | '.join(dict_1[4]) + ' |')
+    print('| ' + ' | '.join(dict_1[5]) + ' |')
+    print('| ' + ' | '.join(dict_1[6]) + ' |')
+    print('| ' + ' | '.join(dict_1[7]) + ' |')
+        
